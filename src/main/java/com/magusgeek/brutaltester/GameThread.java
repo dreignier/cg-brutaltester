ade1574ec369f3a43e917d955f814e2fb6cb2235
David Reignier <david.reignier@gmail.com>
2017-03-30 13:28:56 +0200

    Advance the work on the GameThread

--- src/main/java/com/magusgeek/brutaltester/GameThread.java
+++ src/main/java/com/magusgeek/brutaltester/GameThread.java
@@ -1,8 +1,6 @@
 package com.magusgeek.brutaltester;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
@@ -21,6 +19,8 @@ public class GameThread extends Thread {
     private int n;
     private List<Process> players;
     private Process referee;
+    private List<Scanner> scanners;
+    private Scanner refereeScanner;
 
     public GameThread(int id, String refereeCmd, List<String> playersCmd, Mutable<Integer> count, PlayerStats[] playerStats, int n) {
         this.id = id;
@@ -52,12 +52,39 @@ public class GameThread extends Thread {
                 referee = new ProcessBuilder(refereeCmd.split(" ")).start();
                 
                 // Spawn players process
-                players = new ArrayList<Process>();
+                players = new ArrayList<>();
                 for (String cmd : playersCmd) {
                     players.add(new ProcessBuilder(cmd.split(" ")).start());
                 }
                 
+                // Plug scanners
+                refereeScanner = new Scanner(referee.getInputStream());
+                scanners = new ArrayList<>();
+                for (Process process : players) {
+                    scanners.add(new Scanner(process.getInputStream()));
+                }
                 
+                String line = refereeScanner.nextLine();
+                boolean input = false;
+                int target = 0;
+                
+                while (!line.startsWith("###End")) {
+                    if (line.startsWith("###Input")) {
+                        // Read all lines from the referee until next command and give it to the targeted process
+                    } else if (line.startsWith("###Output")) {
+                        // Read x liens from the targeted process and give to the referee
+                    }
+                }
+                
+                // End of the game
+                String[] params = line.split(" ");
+                for (int i = 1; i < params.length; ++i) {
+                    for (char c : params[i].toCharArray()) {
+                        playerStats[Character.getNumericValue(c)].add(i - 1);
+                    }
+                }
+                
+                destroyAll();
             } catch (Exception exception) {
                 LOG.error("Exception in GameThread " + id, exception);
                 destroyAll();
@@ -66,7 +93,16 @@ public class GameThread extends Thread {
     }
     
     private void destroyAll() {
-        players.forEach(p -> p.destroy());
+        if (scanners != null) {
+            scanners.forEach(s -> s.close());
+        
+        if (refereeScanner != null) {
+            refereeScanner.close();
+        }
+        
+        if (players != null) {
+            players.forEach(p -> p.destroy());
+        }
         
         if (referee != null) {
             referee.destroy();
