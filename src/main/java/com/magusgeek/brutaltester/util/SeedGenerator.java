454233c1649c32b9c60bfe8f907898d679e53c79
Marchete <marchena.carlos@gmail.com>
2018-01-10 12:29:22 -0600

    New Parameter: Initial Seed

--- src/main/java/com/magusgeek/brutaltester/util/SeedGenerator.java
+++ src/main/java/com/magusgeek/brutaltester/util/SeedGenerator.java
@@ -7,7 +7,14 @@ public class SeedGenerator {
     private static Random random = new Random();
     private static int seed = 0;
     private static int usedCount = 0;
-
+    public static boolean repeteableTests = false;
+    public static void initialSeed(long newSeed){
+        random = new Random(newSeed);
+        repeteableTests = true;
+    }
+    public static synchronized int nextSeed() {
+        return random.nextInt(Integer.MAX_VALUE);
+    }
     public static synchronized int[] getSeed(int playerCount) {
         usedCount %= playerCount;
         if (usedCount == 0) {
